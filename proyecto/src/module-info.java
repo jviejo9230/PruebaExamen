7ecc59e47adeed75af42c75da051c4a886b1fc85
jviejo9230 <jviejo9230@ceuandalucia.es>
2023-06-14 12:43:32 +0200

    aa

---
+++ proyecto/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author JVB
+ *
+ */
+module proyecto {
+}
---
+++ proyecto/src/paquete/clase.java
@@ -0,0 +1,8 @@
+package paquete;
+
+public class clase {
+	public static void main(String[] args) {
+		
+		System.out.println("Esto lo subo desde eclipse");
+	}
+}
